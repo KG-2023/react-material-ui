dac8c21c013218367e5a511f60387fe584fe4bfd
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-09 17:38:04 +0800

    Add themeProvider - WIP

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,11 +4,39 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import {AccessTime} from '@mui/icons-material';
 import Rating from '@mui/material/Rating';
+import {createTheme, ThemeProvider} from "@mui/material/";
+
+const theme = createTheme({
+    component:{
+        MuiTypography: {
+            variants: [
+                {
+                    props: {
+                        variant: "body2",
+                        style:{
+                            fontSize: 11,
+                        }
+                    },
+                    props: {
+                        variant: "body3",
+                
+                    },
+                    style:{
+                        fontSize: 9,
+                    }
+                }
+            ]
+        }
+    }
+})
+
 
 const TourCard = () =>{
 
     return (
         <Grid item xs={3}>
+            <ThemeProvider theme={theme}>
+            <Paper elevation={10}>
             <img className="paperImage" src="../images/pexels-balaji-srinivasan-3936815.jpg" alt="Tabung Hampi"/>
             <Box paddingX={1}>
                 <Typography variant="subtitle1" component="h2">
@@ -30,9 +58,19 @@ const TourCard = () =>{
                 <Typography variant="body2" component="p" marginLeft={0.5}>
                     4.5
                 </Typography>
+                <Typography variant="body3" component="p" marginLeft={1.5}>
+                    (500 reviews)
+                </Typography>
             </Box>
-            
-            <Paper elevation={10}> Hello World!</Paper>
+            <Box>
+                <Typography variant="h6" component="h3" marginLeft={.5}>
+                    From Php 1000
+                </Typography>
+
+            </Box>
+          
+             </Paper>
+            </ThemeProvider >
         </Grid>
         
     )
