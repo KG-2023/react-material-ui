8c9649fe95861e253d38f89bdf0e07f0bc4318d5
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-09 17:24:34 +0800

    Add Rating precision component

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,15 +1,37 @@
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography'
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import {AccessTime} from '@mui/icons-material';
+import Rating from '@mui/material/Rating';
 
 const TourCard = () =>{
 
     return (
         <Grid item xs={3}>
             <img className="paperImage" src="../images/pexels-balaji-srinivasan-3936815.jpg" alt="Tabung Hampi"/>
-            <Typography variant="subtitle1" component="h2">
-                Tabung Hampi
-            </Typography>
+            <Box paddingX={1}>
+                <Typography variant="subtitle1" component="h2">
+                    Tabung Hampi
+                </Typography>
+            </Box>
+            <Box sx={{
+                display:"flex",
+                alignItems: "center"
+            }}>
+                <AccessTime sx={{width: 12.5}}/>
+                <Typography variant="body2" component="p" marginLeft={0.5}>
+                    5 hours
+                </Typography>
+            </Box>
+            <Box marginTop={3} 
+            sx={{display:"flex", alignItems:"center"}}>
+                <Rating name="read-only" value={4.5} readOnly  precision={0.5} />
+                <Typography variant="body2" component="p" marginLeft={0.5}>
+                    4.5
+                </Typography>
+            </Box>
+            
             <Paper elevation={10}> Hello World!</Paper>
         </Grid>
         
