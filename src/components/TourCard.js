bc3ee13d1a69a58443c8e48df998ccdcc4541a1a
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-09 16:56:52 +0800

    Setup typography variang subtilte

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,10 +1,15 @@
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography'
 
 const TourCard = () =>{
 
     return (
         <Grid item xs={3}>
+            <img className="paperImage" src="../images/pexels-balaji-srinivasan-3936815.jpg" alt="Tabung Hampi"/>
+            <Typography variant="subtitle1" component="h2">
+                Tabung Hampi
+            </Typography>
             <Paper elevation={10}> Hello World!</Paper>
         </Grid>
         
