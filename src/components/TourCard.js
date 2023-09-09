16a45e5cac70c02366e41d3e5ec02ef116b6b6c7
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-09 16:39:14 +0800

    Add tourcards test grid

---
+++ src/components/TourCard.js
@@ -0,0 +1,15 @@
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const TourCard = () =>{
+
+    return (
+        <Grid item xs={3}>
+            <Paper elevation={10}> Hello World!</Paper>
+        </Grid>
+        
+    )
+    
+}
+
+export default TourCard;
